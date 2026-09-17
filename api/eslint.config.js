9de8ee45123988c27611227e9d8451158c394d9d
ST10434031-Cameron-Naidoo <st10434031@myemeris.edu.za>
2026-09-17 15:44:25 +0200

    added jest global var to eslint.config

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,6 +1,6 @@
 const js = require('@eslint/js');
-
 const globals = require('globals');
+
 module.exports = [
  js.configs.recommended,
  {
@@ -10,6 +10,7 @@ module.exports = [
  sourceType: 'commonjs',
  globals: {
  ...globals.node
+ ...globals.jest
  }
  },
  rules: {
