b68b1081a9af676f1d0c74e5a43beef3f73a65d3
ST10434031-Cameron-Naidoo <st10434031@myemeris.edu.za>
2026-09-17 15:37:11 +0200

    added ci pipeline: .github/workflows/ci.yml

---
+++ api/eslint.config.js
@@ -0,0 +1,22 @@
+const js = require('@eslint/js');
+
+const globals = require('globals');
+module.exports = [
+ js.configs.recommended,
+ {
+ files: ['**/*.js'],
+ languageOptions: {
+ ecmaVersion: 'latest',
+ sourceType: 'commonjs',
+ globals: {
+ ...globals.node
+ }
+ },
+ rules: {
+ 'no-unused-vars': 'warn',
+ 'no-console': 'off',
+ 'eqeqeq': 'error',
+ 'curly': 'error'
+ }
+ }
+]
